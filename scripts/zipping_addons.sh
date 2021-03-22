2b373459bf1f6bbf908a2c7a12e1bbd7e264bf46
AlexPoilrouge <alexandre.hurstel@mailoo.org>
2021-03-22 12:06:10 +0100

    minor improvement on zipping addons process

--- scripts/zipping_addons.sh
+++ scripts/zipping_addons.sh
@@ -12,21 +12,24 @@ gen_addons_zip(){
 
     _DL_ADDONS_DIR="${_WORK_DIR}/dl"
     _BASE_ADDONS_DIR="${_WORK_DIR}/Packs"
+    _TMP_ZIP="${_WORK_DIR}/tmp_strashbot_addons.zip"
     _ZIP_ARCHIVE="${_WORK_DIR}/strashbot_addons.zip"
     _README="${_WORK_DIR}/README.txt"
 
-    rm -f "${_ZIP_ARCHIVE}" 2>/dev/null 2>&1
+    rm -f "${_TMP_ZIP}" 2>/dev/null 2>&1
     echo "These addons are to be copied in the 'DOWNLOAD' folder of your SRB2Kart folder…" > "${_README}"
-    zip "${_ZIP_ARCHIVE}" -j "${_README}" >/dev/null 2>&1
+    zip "${_TMP_ZIP}" -j "${_README}" >/dev/null 2>&1
     rm -f "${_README}" 2>/dev/null 2>&1
 
     ( ls_restricted "${_DL_ADDONS_DIR}" ) | while read -r L; do
-        zip -jur "${_ZIP_ARCHIVE}" "${L}" >/dev/null 2>&1
+        zip -jur "${_TMP_ZIP}" "${L}" >/dev/null 2>&1
     done;
 
     ( ls_restricted "${_BASE_ADDONS_DIR}" ) | while read -r L; do
-        zip -jur "${_ZIP_ARCHIVE}" "${L}" >/dev/null 2>&1
+        zip -jur "${_TMP_ZIP}" "${L}" >/dev/null 2>&1
     done;
+
+    mv "${_TMP_ZIP}" "${_ZIP_ARCHIVE}"
 }
 
 gen_addons_zip "$1"
